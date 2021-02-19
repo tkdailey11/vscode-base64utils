8ef562a4cadffe0383241b30a14d40b15a32d3c2
Tyler Dailey <tkdailey11@gmail.com>
2021-02-18 19:42:45 -0700

    Cleaned up configurations options and README

--- extension.js
+++ extension.js
@@ -1,23 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "testext" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.b64encode', function () {
+	let encodeCommand = vscode.commands.registerCommand('extension.b64encode', function () {
 		var editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return; // No open text editor
@@ -38,11 +26,10 @@ function activate(context) {
 		if(inPlace) {
 			editor.edit(builder => builder.replace(selection, encoded))
 		}
-		
 	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(encodeCommand);
 
-	disposable = vscode.commands.registerCommand('extension.b64decode', function(){
+	let decodeCommand = vscode.commands.registerCommand('extension.b64decode', function(){
 		var editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return; // No open text editor
@@ -65,7 +52,7 @@ function activate(context) {
 			editor.edit(builder => builder.replace(selection, decoded))
 		}
 	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(decodeCommand);
 }
 
 // this method is called when your extension is deactivated
